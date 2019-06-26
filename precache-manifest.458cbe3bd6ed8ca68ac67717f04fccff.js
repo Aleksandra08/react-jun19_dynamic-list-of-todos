fd697e85f5b31f854d1c3e95ac2bfbd084c58ae6
mgrinko <glukym@gmail.com>
2019-06-26 19:26:08 +0300

    Updates

---
+++ precache-manifest.458cbe3bd6ed8ca68ac67717f04fccff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2b0fcf15b51bc7f494d10ab12a0f8a",
+    "url": "/react-jun19_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "1f5d5702a8cfa822eb3f",
+    "url": "/react-jun19_dynamic-list-of-todos/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-jun19_dynamic-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1f5d5702a8cfa822eb3f",
+    "url": "/react-jun19_dynamic-list-of-todos/static/js/main.5a7a4a71.chunk.js"
+  },
+  {
+    "revision": "aaf2a9ecee1a78338d54",
+    "url": "/react-jun19_dynamic-list-of-todos/static/js/runtime~main.8bd6fae3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f3f05dde46505fbf2d27d6abb465933.js"
+  "/react-jun19_dynamic-list-of-todos/precache-manifest.458cbe3bd6ed8ca68ac67717f04fccff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-jun19_dynamic-list-of-todos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
